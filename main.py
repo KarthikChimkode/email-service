d39e47abc9e56635ec1f7e5460a234fd89cd8c88
KarthikChimkode <karthikchimkode@gmail.com>
2026-06-26 08:25:33 +0530

    created .env 14 done

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
+
+load_dotenv()
+
+EMAIL=os.getenv("EMAIL")
+PASSWORD=os.getenv("PASSWORD")
+    
 
 @app.get("/")
 def home():
