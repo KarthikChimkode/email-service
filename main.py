5279d6660f179c81bd731467c5a3ded434c63266
Karthik <99889801+KarthikChimkode@users.noreply.github.com>
2026-06-26 17:13:20 +0530

    Sending mail to actual real emails

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 from dotenv import load_dotenv
+import smtplib
+from email.mime.text import MIMEText
 import os
 
 app = FastAPI()
@@ -22,6 +24,32 @@ class EmailRequest(BaseModel):
     sub: str 
     body: str
 
+def send_actual_email(to, subject, body, cc):
+
+    msg = MIMEText(body)
+    
+    msg["Subject"]=subject
+    msg["From"]=EMAIL
+    msg["To"]=to
+    msg["Cc"]=cc 
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+
+        server.starttls()
+
+        server.login(EMAIL, PASSWORD)
+
+        server.send_message(msg)
+
+
 @app.post("/send_email")
 def send_email(data: EmailRequest):
-    return {"recieved": data}
+
+    send_actual_email(
+        data.to,
+        data.sub,
+        data.body,
+        data.cc
+    )
+
+    return {"recieved": "Email sent"}
